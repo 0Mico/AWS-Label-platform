bb18da30d63f03057641cb53fb02e2b62d04d9b6
Mico <michele.randazzo0100@gmail.com>
2025-10-29 17:20:43 +0100

    start ecs task

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -178,7 +178,7 @@ class CdkStack(Stack):
             "LabelAppService",
             cluster = cluster,
             task_definition = task_definition,
-            desired_count = 0,  # How many containers to run
+            desired_count = 1,  # How many containers to run
             service_name = "my-scraper-service"
         )
 
